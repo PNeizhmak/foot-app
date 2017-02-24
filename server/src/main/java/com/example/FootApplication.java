da17c210f06df5057d9cb16a68b8ebee471a34a6
Pavel Neizhmak <PavelN@playtika.com>
2017-02-24 12:01:52 +0300

    initial project structure

---
+++ server/src/main/java/com/example/FootApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pavel Neizhmak
+ */
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class
+})
+public class FootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootApplication.class, args);
+    }
+}
+
+
---
+++ server/src/test/java/com/example/FootApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Pavel Neizhmak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
