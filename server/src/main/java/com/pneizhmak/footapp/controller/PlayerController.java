850de4631d10cfd545e17548fc472feb678888fa
Pavel Neizhmak <PavelN@playtika.com>
2017-03-17 10:05:07 +0300

    refactoring

--- server/src/main/java/com/pneizhmak/footapp/controller/PlayerController.java
+++ server/src/main/java/com/pneizhmak/footapp/controller/PlayerController.java
@@ -5,7 +5,6 @@ import com.pneizhmak.footapp.db.model.PlayerPosition;
 import com.pneizhmak.footapp.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
