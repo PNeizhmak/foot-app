04bddd7ba89d9883af4d21b4f47d8a9a538b21aa
Pavel Neizhmak <PavelN@playtika.com>
2017-03-19 00:38:44 +0300

    add Position and PlayerProfile DB support

--- server/src/main/java/com/pneizhmak/footapp/db/schema/football.sql
+++ server/src/main/java/com/pneizhmak/footapp/db/schema/football.sql
@@ -2,7 +2,7 @@
 SQLyog Ultimate v12.14 (64 bit)
 MySQL - 5.7.13-log : Database - football
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -16,18 +16,23 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`football` /*!40100 DEFAULT CHARACTER SE
 
 USE `football`;
 
-/*Table structure for table `hibernate_sequence` */
+/*Table structure for table `player_profile` */
 
-DROP TABLE IF EXISTS `hibernate_sequence`;
+DROP TABLE IF EXISTS `player_profile`;
 
-CREATE TABLE `hibernate_sequence` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `hibernate_sequence` */
-
-insert  into `hibernate_sequence`(`next_val`) values 
-(6);
+CREATE TABLE `player_profile` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `player_id` int(3) NOT NULL,
+  `position_id` int(3) NOT NULL,
+  `weight` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK7rscyl2qlky12b5f0ts4l8aq6` (`position_id`),
+  KEY `FKobffu1jp354v25sul0btcqcpy` (`player_id`),
+  CONSTRAINT `FK7rscyl2qlky12b5f0ts4l8aq6` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`),
+  CONSTRAINT `FKehou6s89rv2k40apw44h5onnw` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`),
+  CONSTRAINT `FKnbg9ajtc7cf7odugkpl9op9wv` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`),
+  CONSTRAINT `FKobffu1jp354v25sul0btcqcpy` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `players` */
 
@@ -37,50 +42,7 @@ CREATE TABLE `players` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
-
-/*Data for the table `players` */
-
-insert  into `players`(`id`,`name`) values 
-(1,'Нейжмак');
-insert  into `players`(`id`,`name`) values 
-(2,'Егошин');
-insert  into `players`(`id`,`name`) values 
-(3,'Титов');
-insert  into `players`(`id`,`name`) values 
-(4,'Рубельский');
-insert  into `players`(`id`,`name`) values 
-(5,'Каморников');
-
-/*Table structure for table `players_position` */
-
-DROP TABLE IF EXISTS `players_position`;
-
-CREATE TABLE `players_position` (
-  `player_id` int(3) NOT NULL,
-  `position_id` int(3) NOT NULL,
-  PRIMARY KEY (`player_id`,`position_id`),
-  KEY `FK7rscyl2qlky12b5f0ts4l8aq6` (`position_id`),
-  CONSTRAINT `FK7rscyl2qlky12b5f0ts4l8aq6` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`),
-  CONSTRAINT `FKnbg9ajtc7cf7odugkpl9op9wv` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `players_position` */
-
-insert  into `players_position`(`player_id`,`position_id`) values 
-(1,2);
-insert  into `players_position`(`player_id`,`position_id`) values 
-(2,2);
-insert  into `players_position`(`player_id`,`position_id`) values 
-(4,2);
-insert  into `players_position`(`player_id`,`position_id`) values 
-(1,3);
-insert  into `players_position`(`player_id`,`position_id`) values 
-(4,4);
-insert  into `players_position`(`player_id`,`position_id`) values 
-(5,4);
-insert  into `players_position`(`player_id`,`position_id`) values 
-(3,5);
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `position` */
 
@@ -90,20 +52,7 @@ CREATE TABLE `position` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
-
-/*Data for the table `position` */
-
-insert  into `position`(`id`,`name`) values 
-(1,'goalkeeper');
-insert  into `position`(`id`,`name`) values 
-(2,'defender');
-insert  into `position`(`id`,`name`) values 
-(3,'midfield');
-insert  into `position`(`id`,`name`) values 
-(4,'forward');
-insert  into `position`(`id`,`name`) values 
-(5,'universal');
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
