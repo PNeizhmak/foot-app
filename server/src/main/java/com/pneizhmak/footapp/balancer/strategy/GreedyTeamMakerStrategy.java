f64bfcec79a0856aeb5e0db6e0895c92f9e76760
MaxEg <maxeg@playtika.com>
2017-07-31 12:22:21 +0300

    Fix Greedy strategy to pick at least one player at each type of position. Add random strategy impl.

--- server/src/main/java/com/pneizhmak/footapp/balancer/strategy/GreedyTeamMakerStrategy.java
+++ server/src/main/java/com/pneizhmak/footapp/balancer/strategy/GreedyTeamMakerStrategy.java
@@ -3,6 +3,8 @@ package com.pneizhmak.footapp.balancer.strategy;
 import java.security.InvalidParameterException;
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.pneizhmak.footapp.balancer.model.GreedyTeamMakerModel;
 import com.pneizhmak.footapp.db.model.PlayerProfile;
@@ -27,12 +29,35 @@ public class GreedyTeamMakerStrategy extends AbstractTeamMakerStrategy<GreedyTea
 
     @Override
     protected PlayerProfile findPlayerProfileToPick() {
-        Optional<PlayerProfile> playerProfile = getModel().getPlayerProfiles().stream().max(Comparator.comparing(PlayerProfile::getWeight));
+        // Pick maximum weight for not existing position in team.
+        // If all positions exist, then pick maximum weight between all positions
+        Stream<PlayerProfile> stream = getModel().getPlayerProfiles().stream();
+        if (existPositionNotFilledInTeam()) {
+            stream = stream
+                .filter(playerProfile1 -> !getModel().getCurrentTeam()
+                                                     .getPlayers()
+                                                     .stream()
+                                                     .collect(Collectors.groupingBy(PlayerProfile::getPosition))
+                                                     .containsKey(playerProfile1.getPosition()));
+        }
+        Optional<PlayerProfile> playerProfile = stream.max(Comparator.comparing(PlayerProfile::getWeight));
         if (playerProfile.isPresent()) {
             return playerProfile.get();
         } else {
             throw new InvalidParameterException("Something went wrong during get max profile.");
         }
+    }
+
+    private boolean existPositionNotFilledInTeam() {
+        return getModel().getPlayerProfiles().stream()
+                         .collect(Collectors.groupingBy(PlayerProfile::getPosition))
+                         .keySet()
+                         .stream()
+                         .filter(position -> !getModel().getCurrentTeam().getPlayers().stream()
+                                                        .collect(Collectors.groupingBy(PlayerProfile::getPosition))
+                                                        .keySet()
+                                                        .contains(position))
+                         .count() > 0;
     }
 
 }
--- server/src/main/java/com/pneizhmak/footapp/balancer/strategy/RandomTeamMakerStrategy.java
+++ server/src/main/java/com/pneizhmak/footapp/balancer/strategy/RandomTeamMakerStrategy.java
@@ -1,11 +1,21 @@
 package com.pneizhmak.footapp.balancer.strategy;
 
+import java.util.Random;
+
 import com.pneizhmak.footapp.balancer.model.AbstractTeamMakerModel;
+import com.pneizhmak.footapp.db.model.PlayerProfile;
 
 public class RandomTeamMakerStrategy extends AbstractTeamMakerStrategy<AbstractTeamMakerModel> {
 
+    private Random random = new Random();
+
     public RandomTeamMakerStrategy(AbstractTeamMakerModel model) {
         super(model);
+    }
+
+    @Override
+    protected PlayerProfile findPlayerProfileToPick() {
+        return getModel().getPlayerProfiles().get(random.nextInt(getModel().getPlayerProfiles().size()));
     }
 
 }
